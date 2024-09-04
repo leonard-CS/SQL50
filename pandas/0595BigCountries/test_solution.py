6b21ab4a8bbbd0c7b1993cad302957a34d5a4eb0
Leonard Chi <leonardochi168@gmail.com>
2024-09-04 13:36:04 +0900

    Pandas submission/solution for 595

---
+++ pandas/0595BigCountries/test_solution.py
@@ -0,0 +1,15 @@
+import unittest
+import pandas as pd
+from solution_v1 import big_countries
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.df = pd.read_csv('data.csv')
+
+    def test_case_1(self):
+        result = big_countries(self.df).reset_index(drop=True)
+        expected = pd.read_csv('pandas/0595BigCountries/test.csv')
+        pd.testing.assert_frame_equal(result, expected)
+
+if __name__ == '__main__':
+    unittest.main()
