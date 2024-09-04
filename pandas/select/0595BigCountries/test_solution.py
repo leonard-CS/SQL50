85ce0eb35ca586da24aa6866f8028d9732b81df4
Leonard Chi <leonardocompsci@gmail.com>
2024-09-05 00:44:30 +0800

    Reorganizing files into pandas/select/

--- pandas/select/0595BigCountries/test_solution.py
+++ pandas/select/0595BigCountries/test_solution.py
@@ -8,7 +8,7 @@ class TestSolution(unittest.TestCase):
 
     def test_case_1(self):
         result = big_countries(self.df).reset_index(drop=True)
-        expected = pd.read_csv('pandas/0595BigCountries/test.csv')
+        expected = pd.read_csv('pandas/select/0595BigCountries/test.csv')
         pd.testing.assert_frame_equal(result, expected)
 
 if __name__ == '__main__':
